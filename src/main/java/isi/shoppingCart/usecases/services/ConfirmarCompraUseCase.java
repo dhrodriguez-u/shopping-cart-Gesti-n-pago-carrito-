91298de9d4dd09eebef76d937ca9f89ad7a819ba
Yeison Ospina <yaospina@unillanos.edu.co>
2026-05-24 15:09:20 -0500

    desacoplar gestion de pago de ConfirmarCompraUseCase aplicando GRASP Low Coupling

--- src/main/java/isi/shoppingCart/usecases/services/ConfirmarCompraUseCase.java
+++ src/main/java/isi/shoppingCart/usecases/services/ConfirmarCompraUseCase.java
@@ -7,11 +7,13 @@ import isi.shoppingCart.entities.Product;
 import isi.shoppingCart.entities.Purchase;
 import isi.shoppingCart.entities.PurchaseItem;
 import isi.shoppingCart.usecases.dto.OperationResult;
+import isi.shoppingCart.usecases.dto.PaymentRequest;
+import isi.shoppingCart.usecases.dto.PaymentResult;
 import isi.shoppingCart.usecases.ports.CartRepository;
 import isi.shoppingCart.usecases.ports.CustomerRepository;
+import isi.shoppingCart.usecases.ports.PaymentGateway;
 import isi.shoppingCart.usecases.ports.ProductRepository;
 import isi.shoppingCart.usecases.ports.PurchaseRepository;
-import isi.shoppingCart.usecases.dto.PaymentResult;
 
 import java.util.List;
 
@@ -20,42 +22,23 @@ public class ConfirmarCompraUseCase {
     private CustomerRepository customerRepository;
     private PurchaseRepository purchaseRepository;
     private ProductRepository productRepository;
+    private PaymentGateway paymentGateway;
 
     public ConfirmarCompraUseCase(CartRepository cartRepository,
                                   CustomerRepository customerRepository,
-                                  PurchaseRepository purchaseRepository, ProductRepository productRepository) {
+                                  PurchaseRepository purchaseRepository,
+                                  ProductRepository productRepository,
+                                  PaymentGateway paymentGateway) {
         this.cartRepository = cartRepository;
         this.customerRepository = customerRepository;
         this.purchaseRepository = purchaseRepository;
         this.productRepository = productRepository;
+        this.paymentGateway = paymentGateway;
     }
-//Integrante 3 - Integrante 2
-private PaymentResult processPayment(double total) {
-
-    if (total <= 3000) {
-
-        return new PaymentResult(
-                true,
-                "Transacción aprobada",
-                total,
-                "TXN-001"
-        );
-    }
-
-    return new PaymentResult(
-            false,
-            "Fondos insuficientes",
-            total,
-            "TXN-002"
-    );
-}
-
-
 
     public OperationResult execute() {
         Cart cart = cartRepository.getCart();
 
-        //Integrante 1
         if (cart == null || !cart.isReadyForPayment()) {
             return OperationResult.fail("El carrito no está listo para iniciar pago.");
         }
@@ -65,7 +48,7 @@ private PaymentResult processPayment(double total) {
         if (customer == null) {
             return OperationResult.fail("No hay cliente registrado.");
         }
-//Intrante 3
+
         List<CartItem> items = cart.getItems();
         int i;
 
@@ -83,14 +66,11 @@ private PaymentResult processPayment(double total) {
 
         double total = cart.getTotal();
 
-        PaymentResult paymentResult = processPayment(total);
+        PaymentRequest paymentRequest = new PaymentRequest(total, customer.getId());
+        PaymentResult paymentResult = paymentGateway.pay(paymentRequest);
 
         if (!paymentResult.isApproved()) {
-
-            return OperationResult.fail(
-                    "Pago rechazado: "
-                            + paymentResult.getMessage()
-            );
+            return OperationResult.fail("Pago rechazado: " + paymentResult.getMessage());
         }
 
         Purchase purchase = new Purchase(purchaseRepository.getNextId(), customer);
